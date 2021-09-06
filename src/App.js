c30b87514c90080e86f59a8793eef38a5c80bacd
Sharon Antony <sharonantony51@gmail.com>
2021-09-06 10:31:47 +0530

    Created UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React, { useState, useEffect } from "react";
+import './App.css'
 
 function App() {
+  const [state, setState] = useState(true);
+  useEffect(() => {
+    console.log("Mounting");
+   
+  });
   return (
-    <div>
-      <h1>To Do App</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>Rect tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
